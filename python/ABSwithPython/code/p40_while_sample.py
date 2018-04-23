a23d112522d9c88b0673ead4045f24b7a402ef6e
kwy8791 <kwy8791+gh@gmail.com>
2018-04-23 21:07:42 +0900

    until page 53.

---
+++ python/ABSwithPython/code/p40_while_sample.py
@@ -0,0 +1,6 @@
+spam = 0
+while spam < 5:
+    print('Hello, world.')
+    spam += 1
+
+
---
+++ python/ABSwithPython/code/p42_yourName.py
@@ -0,0 +1,5 @@
+name = ''
+while name != 'kwy8791':
+    print('あなたの名前を入力して下さい。')
+    name = input()
+print('どうも')
